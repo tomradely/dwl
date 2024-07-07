d7d1d322e43ace436ef3fc7fc33c70ee8d24b510
tomradely <mcnabb14@gmail.com>
2024-07-07 19:23:53 +0100

    added window float rules for relevant applications

--- config.def.h
+++ config.def.h
@@ -56,6 +56,11 @@ static const Rule rules[] = {
 	{ "chromium", NULL,	  0,		0,	     1,		   -1 },
 	{ "vlc",      NULL,       0,       	1,           1,		   -1 },
 	{ "okular",   NULL,	  0,		0,	     1,		   -1 },
+	{ "pwvucontrol", NULL,    0,       	1,           0,		   -1 },
+	{ "dolphin",  NULL,       0,       	1,           0,		   -1 },
+	{ "elisa",    NULL,       0,       	1,           0,		   -1 },
+	{ "lximage-qt", NULL,     0,       	1,           0,		   -1 },
+	{ "kasts",    NULL,       0,       	1,           0,		   -1 },
 };
 
 /* layout(s) */
