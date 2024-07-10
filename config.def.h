402790f3448ef75dbf33ba3c9a5c613cfff8207e
tomradely <mcnabb14@gmail.com>
2024-07-10 03:41:43 +0100

    added rule for blueman to float

--- config.def.h
+++ config.def.h
@@ -57,6 +57,7 @@ static const Rule rules[] = {
 	{ "vlc",      NULL,       0,       	1,           1,		   -1 },
 	{ "okular",   NULL,	  0,		0,	     1,		   -1 },
 	{ "pwvucontrol", NULL,    0,       	1,           0,		   -1 },
+	{ "blueman",  NULL,       0,       	1,           0,		   -1 },
 	{ "dolphin",  NULL,       0,       	1,           0,		   -1 },
 	{ "elisa",    NULL,       0,       	1,           0,		   -1 },
 	{ "lximage-qt", NULL,     0,       	1,           0,		   -1 },
