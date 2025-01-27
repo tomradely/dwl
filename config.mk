3ce58c52db9b5fccbaacbc61559ec01c90ac0941
tomradely <mcnabb14@gmail.com>
2025-01-27 12:15:14 -0500

    remembered to enable xwayland

--- config.mk
+++ config.mk
@@ -8,10 +8,7 @@ PREFIX = /usr/local
 MANDIR = $(PREFIX)/share/man
 DATADIR = $(PREFIX)/share
 
-XWAYLAND =
-XLIBS =
 # Uncomment to build XWayland support
-#XWAYLAND = -DXWAYLAND
-#XLIBS = xcb xcb-icccm
-
+XWAYLAND = -DXWAYLAND
+XLIBS = xcb xcb-icccm
 CC = gcc
